5eb16b52bc6604d00b0f9679fc74f85c2c413c06
4utomator <mohammad@stilyoapps.com>
2022-09-03 13:46:49 +0600

    first

---
+++ features/first_test.feature
@@ -0,0 +1,12 @@
+@all
+Feature: my feature file
+
+  @1
+  Scenario: this is trial test to do
+   Given I am on the homepage
+    And I mark the test as passed
+
+  @2
+  Scenario: this is trial test to do marking as 2
+   Given I am on the homepage
+    And I mark the test as passed
---
+++ features/steps/first_test.py
@@ -0,0 +1,19 @@
+from behave import *
+
+
+
+
+@given("I am on the homepage")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
+
+
+@step("I mark the test as passed")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
